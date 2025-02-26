38b83cc67a11aa3bdde445780e2c983b3ea4ca4e
Brandon Brooks <134101686+bbrooks37@users.noreply.github.com>
2025-02-26 07:32:46 -0600

    Add files via upload

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -9,10 +9,10 @@ function AboutPage() {
         Hello! I'm Brandon Brooks, the owner and operator of Brandon Brooks Handyman Services. I'm dedicated to providing reliable and high-quality handyman services to homeowners in Lakeland, Florida, and the surrounding areas.
       </p>
       <p>
-        With years of experience in home repairs and maintenance, I take pride in my attention to detail and commitment to customer satisfaction. I understand the importance of having a trustworthy handyman you can rely on, and I strive to exceed your expectations with every job.
+        With years of experience in yard repairs and maintenance, I take pride in my attention to detail and commitment to customer satisfaction. I understand the importance of having a trustworthy handyman you can rely on, and I strive to exceed your expectations with every job.
       </p>
       <p>
-        My goal is to make your life easier by taking care of those tasks around your home that you don't have the time or expertise to handle. Whether it's yard cleanup, moving assistance, irrigation repairs, or general handy services, I'm here to help.
+        My goal is to make your life easier by taking care of those tasks around your home that you don't have the time or expertise to handle. Whether it's yard cleanup, moving assistance, mulching, repairs, or general handy services, I'm here to help.
       </p>
       <p>
         I'm passionate about serving my local community and building lasting relationships with my clients. Thank you for considering Brandon Brooks Handyman Services for your home repair needs!
